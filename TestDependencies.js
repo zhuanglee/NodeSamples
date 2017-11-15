a881545d87fa6cb4b92f9b2d143c0f113ceb1def
zhuanglee <devlzh93@gmail.com>
2017-11-15 18:39:20 +0800

    移除安装本地库的测试代码

--- TestDependencies.js
+++ TestDependencies.js
@@ -5,14 +5,4 @@ const Sequelize = require('sequelize');
 const bluebird = require('bluebird');
 const redis = bluebird.promisifyAll(require('redis'));
 const redisClient = redis.createClient();
-// 加载本地库
-const commonUtil = require('common-util');
-const mongodbUtil = require('mongodb-util');
-const mysqlUtil = require('mysql-util');
-const redisUtil = require('redis-util');
-const sqlUtil = require('sql-util');
-const ormUtil= require('orm-util');
-
-// TODO 测试
-console.log(commonUtil.getType(new Date()));
 console.log('Hello world');
